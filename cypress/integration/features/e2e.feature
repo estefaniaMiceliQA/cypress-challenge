3fca8f9ca69b59947ae04243e97310334c7a46c5
MartinSorani <martin.sorani@gmail.com>
2025-03-06 00:38:00 -0300

    Added e2e test case

---
+++ cypress/integration/features/e2e.feature
@@ -0,0 +1,18 @@
+Feature: End-to-End Purchase Flow
+
+  Scenario: User completes a full purchase
+    Given I visit the home page
+    When I click on the first product
+    Then I should be on the product page
+
+    When I add the product to the cart
+    Then I should be redirected to the cart page
+    And I should see the added product in the cart
+
+    When I proceed to checkout
+    Then I should be on the checkout page
+
+    When I enter name "John Doe"
+    And I enter email "john.doe@example.com"
+    And I submit the form
+    Then I should see a confirmation message "Thank you for your purchase!"
---
+++ cypress/integration/step_definitions/e2e.js
@@ -0,0 +1,36 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import './cart';
+import './checkout';
+
+Given('I visit the home page', () => {
+  cy.visit('/');
+});
+
+When('I click on the first product', () => {
+  cy.get('ul li a').first().click();
+});
+
+Then('I should be on the product page', () => {
+  cy.url().should('include', '/product/');
+  cy.get('h1').should('exist');
+});
+
+When('I add the product to the cart', () => {
+  cy.get('button').contains('Add to Cart').click();
+});
+
+Then('I should be redirected to the cart page', () => {
+  cy.url().should('include', '/cart');
+});
+
+Then('I should see the added product in the cart', () => {
+  cy.get('ul li').should('have.length.at.least', 1);
+});
+
+When('I proceed to checkout', () => {
+  cy.get('a').contains('Proceed to Checkout').click();
+});
+
+Then('I should be on the checkout page', () => {
+  cy.url().should('include', '/checkout');
+});
