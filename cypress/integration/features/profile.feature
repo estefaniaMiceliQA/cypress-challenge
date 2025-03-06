e0f269e9065ee8b4325869b70c0e037b895d0436
MartinSorani <martin.sorani@gmail.com>
2025-03-05 22:49:51 -0300

    Added profile test cases

--- cypress/integration/features/profile.feature
+++ cypress/integration/features/profile.feature
@@ -1,3 +1,20 @@
 Feature: Profile Page
-  Scenario: User views their profile
+
+  Scenario Outline: User sees their profile information correctly
     Given I visit the profile page
+    Then I should see the profile header
+    And I should see the user name "<name>"
+    And I should see the user email "<email>"
+
+  Examples:
+    | name     | email                 |
+    | John Doe | john.doe@example.com  |
+
+  Scenario: Profile information should not be empty
+    Given I visit the profile page
+    Then The user name should not be empty
+    And The user email should not be empty
+
+  Scenario: Profile page should have correct title
+    Given I visit the profile page
+    Then The browser title should be "Cypress Challenge"
--- cypress/integration/step_definitions/profile.js
+++ cypress/integration/step_definitions/profile.js
@@ -3,3 +3,27 @@ import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 Given('I visit the profile page', () => {
   cy.visit('/profile');
 });
+
+Then('I should see the profile header', () => {
+  cy.get('h1').should('contain', 'Your Profile');
+});
+
+Then('I should see the user name {string}', (expectedName) => {
+  cy.get('p').contains('Name:').should('contain', expectedName);
+});
+
+Then('I should see the user email {string}', (expectedEmail) => {
+  cy.get('p').contains('Email:').should('contain', expectedEmail);
+});
+
+Then('The user name should not be empty', () => {
+  cy.get('p').contains('Name:').invoke('text').should('match', /Name:\s\S+/);
+});
+
+Then('The user email should not be empty', () => {
+  cy.get('p').contains('Email:').invoke('text').should('match', /Email:\s\S+@\S+\.\S+/);
+});
+
+Then('The browser title should be {string}', (expectedTitle) => {
+  cy.title().should('eq', expectedTitle);
+});
