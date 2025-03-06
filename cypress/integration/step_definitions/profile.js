7349fbc17a91622d73dd08e193056feaed867011
MartinSorani <martin.sorani@gmail.com>
2025-03-06 04:50:24 -0300

    Simplified selectors for profile.js

--- cypress/integration/step_definitions/profile.js
+++ cypress/integration/step_definitions/profile.js
@@ -1,18 +1,21 @@
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import './common';
 
+const userNameField = () => cy.get('p').contains('Name:');
+const userEmailField = () => cy.get('p').contains('Email:');
+
 Then('I should see the user name {string}', (expectedName) => {
-  cy.get('p').contains('Name:').should('contain', expectedName);
+    userNameField().should('contain', expectedName);
 });
 
 Then('I should see the user email {string}', (expectedEmail) => {
-  cy.get('p').contains('Email:').should('contain', expectedEmail);
+    userEmailField().should('contain', expectedEmail);
 });
 
 Then('The user name should not be empty', () => {
-  cy.get('p').contains('Name:').invoke('text').should('match', /Name:\s\S+/);
+    userNameField().invoke('text').should('match', /Name:\s\S+/);
 });
 
 Then('The user email should not be empty', () => {
-  cy.get('p').contains('Email:').invoke('text').should('match', /Email:\s\S+@\S+\.\S+/);
+    userEmailField().invoke('text').should('match', /Email:\s\S+@\S+\.\S+/);
 });
